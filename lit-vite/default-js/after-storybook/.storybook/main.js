482fee9401757bb039e6ee615163b2491c58c492
Yann Braga <yannbf@gmail.com>
2023-04-21 15:58:52 +0200

    fix lit vite templates

--- lit-vite/default-js/after-storybook/.storybook/main.js
+++ lit-vite/default-js/after-storybook/.storybook/main.js
@@ -1,9 +1,9 @@
-/** @type { import('@storybook/web-components-webpack5').StorybookConfig } */
+/** @type { import('@storybook/web-components-vite').StorybookConfig } */
 const config = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
   framework: {
-    name: '@storybook/web-components-webpack5',
+    name: '@storybook/web-components-vite',
     options: {},
   },
   docs: {
--- lit-vite/default-ts/after-storybook/.storybook/main.ts
+++ lit-vite/default-ts/after-storybook/.storybook/main.ts
@@ -1,13 +1,13 @@
-import type { StorybookConfig } from '@storybook/web-components-webpack5';
+import type { StorybookConfig } from '@storybook/web-components-vite'
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
   framework: {
-    name: '@storybook/web-components-webpack5',
+    name: '@storybook/web-components-vite',
     options: {},
   },
   docs: {
     autodocs: 'tag',
   },
-};
-export default config;
+}
+export default config
