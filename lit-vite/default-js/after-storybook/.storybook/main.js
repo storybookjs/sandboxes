496797ad5a02ab64d3aa6e5b115d76b7bc4b2209
Yann Braga <yannbf@gmail.com>
2023-04-24 08:05:19 +0200

    fix lit-vite templates

--- lit-vite/default-js/after-storybook/.storybook/main.js
+++ lit-vite/default-js/after-storybook/.storybook/main.js
@@ -1,13 +1,13 @@
-/** @type { import('@storybook/web-components-webpack5').StorybookConfig } */
+/** @type { import('@storybook/web-components-vite').StorybookConfig } */
 const config = {
-  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
   framework: {
-    name: '@storybook/web-components-webpack5',
+    name: "@storybook/web-components-vite",
     options: {},
   },
   docs: {
-    autodocs: 'tag',
+    autodocs: "tag",
   },
 };
 export default config;
