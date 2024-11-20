346cd9f10ba23e8bb068f9df04b351c36819f32d
Valentin Palkovic <valentin@chromatic.com>
2024-11-20 14:31:21 +0100

    Add angular-vite manually

---
+++ angular-vite/default-ts/after-storybook/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  template: `
+    <h1>Welcome to {{title}}!</h1>
+
+    <router-outlet />
+  `,
+  styles: [],
+})
+export class AppComponent {
+  title = 'angular-latest';
+}
