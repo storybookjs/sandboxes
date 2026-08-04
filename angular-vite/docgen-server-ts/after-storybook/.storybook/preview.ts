d2565410a5de10bf2e4a523b57170f0be08022cf
Valentin Palkovic <valentinpalkovic@mbpvonvalentin2.speedport.ip>
2026-08-04 14:41:58 +0200

    10.6.0-alpha.4 - Tue Aug 04 2026 - bb5ab56faaaa
    Check the diff here: https://github.com/storybookjs/storybook/compare/bb5ab56faaaa...29a0e16f510a
    
    List of included PRs since previous version:
    - https://github.com/storybookjs/storybook/pull/35660 (Insik-Han/fix/tanstack-standalone-index-clone-id)
    - https://github.com/storybookjs/storybook/pull/35742 (fix/pnpm-dlx-allow-build-esbuild)
    - https://github.com/storybookjs/storybook/pull/35713 (jeppe-cursor/setup-dev-environment-1230)

---
+++ angular-vite/docgen-server-ts/after-storybook/.storybook/preview.ts
@@ -0,0 +1,24 @@
+import type { Preview } from '@storybook/angular-vite';
+import { setCompodocJson } from '@storybook/addon-docs/angular';
+import docJson from '../documentation.json';
+setCompodocJson(docJson);
+
+const preview: Preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+
+    a11y: {
+      // 'todo' - show a11y violations in the test UI only
+      // 'error' - fail CI on a11y violations
+      // 'off' - skip a11y checks entirely
+      test: 'todo',
+    },
+  },
+};
+
+export default preview;
